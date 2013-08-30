0b84c411715b53c7f9235c7d015aa658c489ab75
Beryl <beryl.schragger@gmail.com>
2013-08-30 10:51:56 -0400

    Committing quiz.

--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,22 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+Array.prototype.sort = function() {
+  var size = this.length;
+  if (size <= 1) {
+    return this;
+  }
+  var pivot_spot = Math.floor((size - 1) / 2);
+  var pivot = this.splice(pivot_spot, 1);
+  var less = [];
+  var greater = [];
+  for (i = 0; i < size - 1; i++) {
+    if (this[i] <= pivot) {
+      less.push(this[i]);
+    } else {
+      greater.push(this[i]);
+    }
+  }
+  return less.sort().concat(pivot, greater.sort());
+}
